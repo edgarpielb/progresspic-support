8be8b2bdcc26d5c234e96a8274c2ae87e777a16c
xCode <edgar.pielbusch@hotmail.com>
2025-09-11 17:50:15 +0200

    Initial Commit

---
+++ ProgressPic/ContentView.swift
@@ -0,0 +1,24 @@
+//
+//  ContentView.swift
+//  ProgressPic
+//
+//  Created by Edgar Pielbusch on 2025-09-11.
+//
+
+import SwiftUI
+
+struct ContentView: View {
+    var body: some View {
+        VStack {
+            Image(systemName: "globe")
+                .imageScale(.large)
+                .foregroundStyle(.tint)
+            Text("Hello, world!")
+        }
+        .padding()
+    }
+}
+
+#Preview {
+    ContentView()
+}
---
+++ ProgressPic/ProgressPicApp.swift
@@ -0,0 +1,17 @@
+//
+//  ProgressPicApp.swift
+//  ProgressPic
+//
+//  Created by Edgar Pielbusch on 2025-09-11.
+//
+
+import SwiftUI
+
+@main
+struct ProgressPicApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
